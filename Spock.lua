4d8335e926a51f39290b1aa099ec0fda87453132
SpockV99 <v84801738@gmail.com>
2025-03-16 13:37:05 -0300

    Update Spock.lua

--- Spock.lua
+++ Spock.lua
@@ -1,1 +1,6 @@
-loadstring(game:httpget("https://raw.githubusercontent.com/Overgustx2/Spock/refs/heads/main/Spock.lua")) 
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/SpockV99/SPOCK-LAND-HUB/refs/heads/main/Games.lua"))()
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
