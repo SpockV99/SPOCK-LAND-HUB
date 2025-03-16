b33ab9b6bb6cf23c8d17e7315c700043d48e4e49
SpockV99 <v84801738@gmail.com>
2025-03-16 13:34:04 -0300

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,4 @@
+local Games = {
+  [2753915549] = "https://raw.githubusercontent.com/Overgustx2/Spock/refs/heads/main/Spock.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/Overgustx2/Spock/refs/heads/main/Spock.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/Overgustx2/Spock/refs/heads/main/Spock.lua", -- Blox Fruit Sea 3
